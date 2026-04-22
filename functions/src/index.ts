63329f2b6d4cb38fe3b0c7c77f56da512e4fa01d
local2local <todd.herron@local2local.ca>
2026-04-21 23:17:14 -0600

    feat(evolution): autonomous code mutation for phase 40.7.21

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,11 @@
-// Test B logic verified.
+import * as admin from "firebase-admin";
+if (admin.apps.length === 0) admin.initializeApp();
+
+export { evolutionOrchestratorV2, ombudsmanValidatorV2, autonomousFixerV2, evolutionProposalFinalizerV2 } from "./logic/evolution";
+export { complianceAgentV2 } from "./logic/compliance";
+export { financeAgentV2, taxWorkerV2, stripeOnboardingWorker } from "./logic/finance";
+export { gpsTelemetryWorkerV2, carrierBoardWorkerV2 } from "./logic/fulfillment";
+export { facilityMatchingWorkerV2 } from "./logic/infrastructure";
+export { unifiedActivityWorkerV2 } from "./logic/orchestration";
+export { deleteSubcollectionV2 } from "./utilities/deleteSubcollection";
+export { listSubcollectionsV2 } from "./utilities/listSubcollections";
