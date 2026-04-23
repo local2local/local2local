9029682726b1d4716eac919628a7371873448010
Todd Herron <todd.c.herron@gmail.com>
2026-04-23 09:29:59 -0600

    fix(cicd): update firebase-tools, add deploy debug flag, and restore telemetry exports v11.42.1.7

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,17 @@
 import * as admin from "firebase-admin";
 if (admin.apps.length === 0) admin.initializeApp();
 
+// L2LAAF Core
 export { evolutionOrchestratorV2, ombudsmanValidatorV2, autonomousFixerV2, evolutionProposalFinalizerV2 } from "./logic/evolution";
+export { ingestWebError, ingestGCPErrors, telemetryAggregatorV2 } from "./logic/telemetry";
+export { unifiedActivityWorkerV2 } from "./logic/orchestration";
+
+// System Workers
 export { complianceAgentV2 } from "./logic/compliance";
 export { financeAgentV2, taxWorkerV2, stripeOnboardingWorker } from "./logic/finance";
 export { gpsTelemetryWorkerV2, carrierBoardWorkerV2 } from "./logic/fulfillment";
 export { facilityMatchingWorkerV2 } from "./logic/infrastructure";
-export { unifiedActivityWorkerV2 } from "./logic/orchestration";
-export { ingestWebError, ingestGCPErrors, telemetryAggregatorV2 } from "./logic/telemetry";
+
+// Utilities
 export { deleteSubcollectionV2 } from "./utilities/deleteSubcollection";
 export { listSubcollectionsV2 } from "./utilities/listSubcollections";
